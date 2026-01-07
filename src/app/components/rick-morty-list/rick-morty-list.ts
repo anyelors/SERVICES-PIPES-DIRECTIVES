8324c4a8e070cd5b249e830cb1d66184f8fb684d
anyelors <anyelors@gmail.com>
2026-01-07 20:11:28 +0100

    Se ajusta #3

--- src/app/components/rick-morty-list/rick-morty-list.ts
+++ src/app/components/rick-morty-list/rick-morty-list.ts
@@ -1,11 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
+import { AsyncPipe, NgIf } from '@angular/common';
 import { RickMorty } from '../../services/rick-morty';
 import { Dato, Personaje } from '../../models/entities';
 import { Loading } from "../loading/loading";
 
 @Component({
   selector: 'app-rick-morty-list',
-  imports: [Loading],
+  imports: [Loading, AsyncPipe, NgIf],
   templateUrl: './rick-morty-list.html',
   styleUrl: './rick-morty-list.css',
 })
@@ -13,25 +14,33 @@ export class RickMortyList implements OnInit {
 
   nPagina: number = 1;
 
-  _rickMortyService:RickMorty = inject(RickMorty);
+  _rickMortyService: RickMorty = inject(RickMorty);
 
-  personajes:Array<Personaje> = [];
+  personajes: Array<Personaje> = [];
 
-    ngOnInit(): void {
+  ngOnInit(): void {
+
+    this.cargarPersonaje();
+  }
+
+  cargarPersonaje(): void {
+    this._rickMortyService.loading$.subscribe((loading) => {
+      console.log('Cargando:', loading);
+    });
     this._rickMortyService.getPersonajes(this.nPagina).subscribe({
 
-        next: (datos:Dato) => {
-          this.personajes = datos.results;
-          console.log(this.personajes);
-        }
-        ,
-        error: (error) => {
-          console.error('Error al cargar los personajes', error);
-        }
-        ,
-        complete: () => {
-          console.log('Carga de personajes completada');
-        }
+      next: (datos: Dato) => {
+        this.personajes = datos.results;
+        console.log(this.personajes);
+      }
+      ,
+      error: (error) => {
+        console.error('Error al cargar los personajes', error);
+      }
+      ,
+      complete: () => {
+        console.log('Carga de personajes completada');
+      }
     });
   }
 
--- src/app/services/rick-morty.ts
+++ src/app/services/rick-morty.ts
@@ -1,18 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Dato } from '../models/entities';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, delay, finalize, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RickMorty {
+  private loadingSubject = new BehaviorSubject<boolean>(false);
+  loading$ = this.loadingSubject.asObservable();
 
-  _http:HttpClient = inject(HttpClient);
+  _http: HttpClient = inject(HttpClient);
 
-  getPersonajes(nPagina:number):Observable<Dato>{
-    //console.log('Página solicitada:', nPagina);
-    return this._http.get<Dato>(`https://rickandmortyapi.com/api/character?page=${nPagina}`);
+  getPersonajes(nPagina: number): Observable<Dato> {
+    console.log('Página solicitada:', nPagina);
+
+    this.loadingSubject.next(true);
+
+    return this._http.get<Dato>(`https://rickandmortyapi.com/api/character?page=${nPagina}`).pipe(
+      delay(1500), // Simula latencia (opcional, solo para ver el loading
+      finalize(() => this.loadingSubject.next(false))
+    );
   }
-  
+
 }
